084bcd570024409a43db115cfc9e50ecec2c7a77
Masahiro C. Kubokawa <2013056112@ust-ics.mygbiz.com>
2018-11-22 20:57:56 +0800

    1

---
+++ app/src/main/java/masahiro/kubokawa/com/diseaseawareness/CoronaryHeartDisease.java
@@ -0,0 +1,14 @@
+package masahiro.kubokawa.com.diseaseawareness;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class CoronaryHeartDisease extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coronary_heart_disease);
+    }
+
+}
