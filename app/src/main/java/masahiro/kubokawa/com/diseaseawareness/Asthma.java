e3735de9cca893b7554ea62a68e50232e60baa06
Masahiro C. Kubokawa <2013056112@ust-ics.mygbiz.com>
2018-11-28 21:36:02 +0800

    1

---
+++ app/src/main/java/masahiro/kubokawa/com/diseaseawareness/Asthma.java
@@ -0,0 +1,14 @@
+package masahiro.kubokawa.com.diseaseawareness;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Asthma extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asthma);
+    }
+
+}
